37c41fc63d860ef06008e6219c7913d9e232d0ad
Sami <hostikka@gmail.com>
2012-07-05 14:36:18 +0300

    1/290 koans

--- koans/about_asserts.py
+++ koans/about_asserts.py
@@ -9,7 +9,7 @@ class AboutAsserts(Koan):
         """
         We shall contemplate truth by testing reality, via asserts.
         """
-        self.assertTrue(False) # This should be true
+        self.assertTrue(True) # This should be true
     
     def test_assert_with_message(self):
         """

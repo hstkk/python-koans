d76d4a2d3b9cc787ce3bb5cd4b45a6d169d20db3
Sami <hostikka@gmail.com>
2012-07-06 22:41:10 +0300

    99/290

--- koans/about_true_and_false.py
+++ koans/about_true_and_false.py
@@ -29,7 +29,7 @@ class AboutTrueAndFalse(Koan):
         self.assertEqual('false stuff', self.truth_value(set()))
 
     def test_blank_strings_are_treated_as_false(self):
-        self.assertEqual(__, self.truth_value(""))
+        self.assertEqual('false stuff', self.truth_value(""))
         
     def test_everything_else_is_treated_as_true(self):
         self.assertEqual(__, self.truth_value(1))

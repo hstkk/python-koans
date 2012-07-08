abc92eb3a66eb5ba09da926129685d8e89acc53d
Sami <hostikka@gmail.com>
2012-07-08 13:37:22 +0300

    156/290

--- koans/about_classes.py
+++ koans/about_classes.py
@@ -90,7 +90,7 @@ class AboutClasses(Koan):
         fido = self.Dog4()
 
         fido.name = "Fido"
-        self.assertEqual(__, fido.name)
+        self.assertEqual('Fido', fido.name)
 
     # ------------------------------------------------------------------
 

8f020c040a62c8b4ecfb64e63bc35b954040a97e
Sami <hostikka@gmail.com>
2012-07-08 12:37:27 +0300

    136/290

--- koans/about_generators.py
+++ koans/about_generators.py
@@ -134,7 +134,7 @@ class AboutGenerators(Koan):
                          
         generator2 = self.yield_tester()
         next(generator2)
-        self.assertEqual(__, next(generator2))
+        self.assertEqual('no value', next(generator2))
 
     def test_send_none_is_equivelant_to_next(self):
         generator = self.yield_tester()

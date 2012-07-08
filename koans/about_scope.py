0d969122e4bb6c751b15a8ec56fef9af40d8378c
Sami <hostikka@gmail.com>
2012-07-08 20:06:53 +0300

    223/290

--- koans/about_scope.py
+++ koans/about_scope.py
@@ -71,7 +71,7 @@ class AboutScope(Koan):
         global counter
         start = counter
         self.increment_using_local_counter(start)
-        self.assertEqual(__, counter == start + 1)
+        self.assertEqual(False, counter == start + 1)
 
     def test_incrementing_with_global_counter(self):
         global counter

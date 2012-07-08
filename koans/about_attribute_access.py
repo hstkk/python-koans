d53ab645771468d48ce6682a3e3a2cf57c9ebf2c
Sami <hostikka@gmail.com>
2012-07-08 23:08:39 +0300

    269/290

--- koans/about_attribute_access.py
+++ koans/about_attribute_access.py
@@ -165,9 +165,9 @@ class AboutAttributeAccess(Koan):
         fanboy.comic = 'The Laminator, issue #1'
         fanboy.pie = 'blueberry'
         
-        self.assertEqual(__, fanboy.a_pie) 
+        self.assertEqual('blueberry', fanboy.a_pie) 
 
-        prefix = '__'
+        prefix = 'my'
         self.assertEqual("The Laminator, issue #1", getattr(fanboy, prefix + '_comic'))
 
     # ------------------------------------------------------------------

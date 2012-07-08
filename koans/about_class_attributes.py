76e8e1bc72c4f42f00b550e58f7d740e270353e2
Sami <hostikka@gmail.com>
2012-07-08 20:36:12 +0300

    244/290

--- koans/about_class_attributes.py
+++ koans/about_class_attributes.py
@@ -26,7 +26,7 @@ class AboutClassAttributes(Koan):
         self.assertEqual(24, len(dir(fido)))
     
     def test_classes_have_methods(self):
-        self.assertEqual(__, len(dir(self.Dog)))
+        self.assertEqual(24, len(dir(self.Dog)))
 
     def test_creating_objects_without_defining_a_class(self):
         singularity = object()        

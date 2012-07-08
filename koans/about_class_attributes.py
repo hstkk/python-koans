36596d148d7c3824b1c458db0a34bca9eb2590b9
Sami <hostikka@gmail.com>
2012-07-08 20:35:20 +0300

    242/290

--- koans/about_class_attributes.py
+++ koans/about_class_attributes.py
@@ -19,7 +19,7 @@ class AboutClassAttributes(Koan):
         self.assertEqual(True, self.Dog.__class__ == type)
        
     def test_classes_are_objects_too(self):
-        self.assertEqual(__, issubclass(self.Dog, object))
+        self.assertEqual(True, issubclass(self.Dog, object))
     
     def test_objects_have_methods(self):
         fido = self.Dog()
